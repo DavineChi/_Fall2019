fa791d5129ce8400629fd9df159a0cc4a0cc26dc
Shannon Fisher <davine_chi@hotmail.com>
2019-08-29 22:49:35 -0500

    Implement programming exercise 4

--- python/edu.metrostate.ICS460/exercise_intro_python_4.py
+++ python/edu.metrostate.ICS460/exercise_intro_python_4.py
@@ -8,3 +8,24 @@ yyyy.
 
 @author: Shannon Fisher
 '''
+from os.path import sep
+calendar_months = ['January',
+                   'February',
+                   'March',
+                   'April',
+                   'May',
+                   'June',
+                   'July',
+                   'August',
+                   'September',
+                   'October',
+                   'November',
+                   'December']
+
+date = str(input('Enter a date (mm/dd/yyyy): '))
+
+month_part = int(date[0:2])
+day_part = date[3:5]
+year_part = date[6:10]
+
+print('The date is ', calendar_months[month_part - 1], ' ', day_part, ', ', year_part, sep="")
