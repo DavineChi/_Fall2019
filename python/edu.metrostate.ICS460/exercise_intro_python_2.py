bcb63f64be498c56e0d8fe383f4a667ecaa198de
Shannon Fisher <davine_chi@hotmail.com>
2019-08-29 21:20:57 -0500

    Implement programming exercise 2

--- python/edu.metrostate.ICS460/exercise_intro_python_2.py
+++ python/edu.metrostate.ICS460/exercise_intro_python_2.py
@@ -11,3 +11,20 @@ The average of the numbers in the list
 
 @author: Shannon Fisher
 '''
+numbers = list()
+sum = 0.0
+
+for i in range(6):
+    num = float(input('Enter a number: '))
+    numbers.append(num)
+
+numbers.sort(key=None, reverse=False)
+
+print('The smallest number is', numbers[0])
+print('The largest number is', numbers[-1])
+
+for number in range(len(numbers)):
+    sum = sum + numbers[number]
+
+print('The sum of the list is', sum)
+print('The average of the list is', (sum / len(numbers)))
