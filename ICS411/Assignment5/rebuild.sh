cd304b7bc65cfe7779acf40c730de8b6e4cb4005
Shannon Fisher <davine_chi@hotmail.com>
2019-11-27 17:11:57 -0600

    Update ICS411 Assignment 5 rebuild script

--- ICS411/Assignment5/rebuild.sh
+++ ICS411/Assignment5/rebuild.sh
@@ -1,4 +1,4 @@
-hadoop fs -rm -R /user/fisher/Assign5/output
+hadoop fs -rm -R /user/fisher/Assign5/output*
 rm assignment5jobs.jar
 jar -cvf assignment5jobs.jar -C bin .
 chmod 777 assignment5jobs.jar
