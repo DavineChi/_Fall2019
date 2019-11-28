c8676cc4e8f15f2709b9be4bff3c1c7549ef701b
Shannon Fisher <davine_chi@hotmail.com>
2019-11-27 21:53:26 -0600

    Update completed documentation

--- ICS411/Assignment5/rebuild.sh
+++ ICS411/Assignment5/rebuild.sh
@@ -1,4 +1,6 @@
 hadoop fs -rm -R /user/fisher/Assign5/output*
+hadoop fs -rm -R /user/fisher/Assign5/job/lib/assignment5jobs.jar
 rm assignment5jobs.jar
 jar -cvf assignment5jobs.jar -C bin .
 chmod 777 assignment5jobs.jar
+hadoop fs -copyFromLocal /home/cloudera/workspace/Assignment5/assignment5jobs.jar /user/fisher/Assign5/job/lib/assignment5jobs.jar
